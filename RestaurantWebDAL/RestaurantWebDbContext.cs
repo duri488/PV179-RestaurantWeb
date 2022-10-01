eef096133da7a9cf2ab45840cadca30850821536
duri488 <lazorik.juraj@gmail.com>
2022-10-01 16:19:04 +0200

    Add models to DbContext

--- RestaurantWebDAL/RestaurantWebDbContext.cs
+++ RestaurantWebDAL/RestaurantWebDbContext.cs
@@ -5,6 +5,13 @@ namespace RestaurantWebDAL
     public class RestaurantWebDbContext : DbContext
     {
         public DbSet<User> User { get; set; }
+        public DbSet<Meal> Meal { get; set; }
+        public DbSet<Drink> Drink { get; set; }
+        public DbSet<Restaurant> Restaurant { get; set; }
+        public DbSet<DailyMenu> DailyMenu { get; set; }
+        public DbSet<WeeklyMenu> WeeklyMenu { get; set; }
+        public DbSet<Localization> Localization { get; set; }
+
 
         public RestaurantWebDbContext(DbContextOptions<RestaurantWebDbContext> options) : base(options)
         {

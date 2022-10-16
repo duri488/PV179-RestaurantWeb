42df20cba9d78a399f9c0e07b8a0c687ed379ce2
duri488 <lazorik.juraj@gmail.com>
2022-10-16 20:54:32 +0200

    Create interfaces for repository UoW and Query

---
+++ RestaurantWebInfrastructure/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantWebInfrastructure.Interfaces
+{
+    internal interface IUnitOfWork : IDisposable
+    {
+        Task Commit();
+    }
+}

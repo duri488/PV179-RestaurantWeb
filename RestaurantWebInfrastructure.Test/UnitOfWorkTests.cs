5db94c338dcf0fc4eb239ed830dee823beefe838
duri488 <lazorik.juraj@gmail.com>
2022-10-17 21:47:28 +0200

    UoW test for transaction fail - not working

--- RestaurantWebInfrastructure.Test/UnitOfWorkTests.cs
+++ RestaurantWebInfrastructure.Test/UnitOfWorkTests.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
+using System.Xml.Linq;
 
 namespace RestaurantWebInfrastructure.Test
 {
@@ -18,7 +19,7 @@ namespace RestaurantWebInfrastructure.Test
         [SetUp]
         public void Setup()
         {
-            string dbName = "test_database_" + DateTime.Now.ToFileTimeUtc();
+            string dbName = "test_database";
             DbContextOptions<RestaurantWebDbContext> options = new DbContextOptionsBuilder<RestaurantWebDbContext>()
                 .UseInMemoryDatabase(databaseName: dbName)
                 .Options;
@@ -37,6 +38,43 @@ namespace RestaurantWebInfrastructure.Test
             }
 
             Assert.That(DbContext.Meal.Contains(pizza), Is.True);
+        }
+
+        [Test]
+        public async Task UoWTransactionFail()
+        {
+            //TODO - not working properly
+            string dbName = "test_database1";
+            DbContextOptions<RestaurantWebDbContext> options = new DbContextOptionsBuilder<RestaurantWebDbContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            var DbContext1 = new RestaurantWebDbContext(options);
+
+            var pizza = new Meal {Id = 1, Name = "pizza", Picture = "Picture", Description = "mnam", Restaurants = new List<Restaurant>() };
+
+            using UnitOfWork Unit1 = new(DbContext1);
+            {
+                Unit1.MealRepository.Insert(pizza);
+                await Unit1.Commit();                
+            }
+
+            var DbContext2 = new RestaurantWebDbContext(options);
+
+            var burger = new Meal { Id = 2, Name = "burger", Picture = "Picture", Description = "mnam", Restaurants = new List<Restaurant>() };
+
+            using UnitOfWork Unit2 = new(DbContext2);
+            {
+                Unit2.MealRepository.Insert(burger);
+                Unit2.MealRepository.Insert(pizza);
+                //await Unit2.Commit();
+                Assert.ThrowsAsync<ArgumentException>(async () => await Unit2.Commit()); //ok
+            }
+
+            var DbContext3 = new RestaurantWebDbContext(options);
+            
+            Assert.That(DbContext3.Meal.Count() == 1, Is.True); //fail
+            Assert.That(DbContext3.Meal.Count() == 2, Is.True); //ok               
         }
     }
 }

c923212d260b7643646a4220e5b4344445d2c893
xbock <xbock@fi.muni.cz>
2022-10-17 18:35:51 +0200

    Fix query tests

--- RestaurantWebInfrastructure.Test/QueryTests.cs
+++ RestaurantWebInfrastructure.Test/QueryTests.cs
@@ -7,7 +7,7 @@ namespace RestaurantWebInfrastructure.Test
 {
     public class QueryTests
     {
-        private readonly RestaurantWebDbContext dbContext;
+        private RestaurantWebDbContext DbContext { get; set; }
 
         [SetUp]
         public void Setup()
@@ -18,9 +18,9 @@ namespace RestaurantWebInfrastructure.Test
                             .UseInMemoryDatabase(databaseName: myDatabaseName)
                             .Options;
 
-            var dbContext = new RestaurantWebDbContext(dbContextOptions);
+            DbContext = new RestaurantWebDbContext(dbContextOptions);
 
-            dbContext.Meal.Add(new Meal
+            DbContext.Meal.Add(new Meal
             {
                 Id = 1,
                 Name = "Rezen",
@@ -29,18 +29,79 @@ namespace RestaurantWebInfrastructure.Test
                 Price = (decimal)100.00,
                 Restaurants = new List<Restaurant>()
             });
+
+            DbContext.SaveChanges();
         }
 
         [Test]
         public void MealExists_QueryWhere_Test()
         {
-            var efquery = new Query<Meal>(dbContext);
+            var efquery = new Query<Meal>(DbContext);
             efquery.Where<string>(a => a == "Rezen", "Name");
             var result = efquery.Execute();
 
             Assert.True(result.Count() == 1);
-
             Assert.True(result.First().Name == "Rezen");
         }
+
+        [Test]
+        public void MealExists_QueryOrderBy_Test()
+        {
+
+            DbContext.Meal.Add(new Meal
+            {
+                Id = 2,
+                Name = "Burger",
+                Description = "Maso v zemli",
+                Picture = "obrazok",
+                Price = (decimal)150.00,
+                Restaurants = new List<Restaurant>()
+            });
+
+            DbContext.SaveChanges();
+
+            var efquery = new Query<Meal>(DbContext);
+            efquery.OrderBy<int>("Id", false);
+            var result = efquery.Execute();
+
+            Assert.True(result.Count() == 2);
+            Assert.True(result.First().Name == "Burger");
+        }
+
+        [Test]
+        public void MealExists_QueryPage_Test()
+        {
+            DbContext.Meal.Add(new Meal
+            {
+                Id = 2,
+                Name = "Burger",
+                Description = "Maso v zemli",
+                Picture = "obrazok",
+                Price = (decimal)150.00,
+                Restaurants = new List<Restaurant>()
+            });
+
+            DbContext.SaveChanges();
+
+            DbContext.Meal.Add(new Meal
+            {
+                Id = 3,
+                Name = "Pizza",
+                Description = "Maso v zemli",
+                Picture = "obrazok",
+                Price = (decimal)150.00,
+                Restaurants = new List<Restaurant>()
+            });
+
+            DbContext.SaveChanges();
+
+            var efquery = new Query<Meal>(DbContext);
+            efquery.Page(1, 2);
+            var result = efquery.Execute();
+
+            Assert.True(result.Count() == 2);
+            Assert.True(result.First().Name == "Rezen");
+        }
+
     }
 }

37741edbd2f62c06dc488b76aa5e1942b659b530
xbock <xbock@fi.muni.cz>
2022-10-22 13:22:07 +0200

    add query tests

--- RestaurantWebInfrastructure.Test/QueryTests.cs
+++ RestaurantWebInfrastructure.Test/QueryTests.cs
@@ -30,6 +30,26 @@ namespace RestaurantWebInfrastructure.Test
                 Restaurants = new List<Restaurant>()
             });
 
+            DbContext.Meal.Add(new Meal
+            {
+                Id = 2,
+                Name = "Burger",
+                Description = "Maso v zemli",
+                Picture = "obrazok",
+                Price = (decimal)140.00,
+                Restaurants = new List<Restaurant>()
+            });
+
+            DbContext.Meal.Add(new Meal
+            {
+                Id = 3,
+                Name = "Pizza",
+                Description = "Cesto so syrom a pretlakom",
+                Picture = "obrazok",
+                Price = (decimal)150.00,
+                Restaurants = new List<Restaurant>()
+            });
+
             DbContext.SaveChanges();
         }
 
@@ -45,14 +65,14 @@ namespace RestaurantWebInfrastructure.Test
         }
 
         [Test]
-        public void Query_OrderBy_HappyPath()
+        public void Query_WhereComplex_HappyPath()
         {
 
             DbContext.Meal.Add(new Meal
             {
-                Id = 2,
-                Name = "Burger",
-                Description = "Maso v zemli",
+                Id = 4,
+                Name = "Rezen",
+                Description = "Maso",
                 Picture = "obrazok",
                 Price = (decimal)150.00,
                 Restaurants = new List<Restaurant>()
@@ -61,21 +81,35 @@ namespace RestaurantWebInfrastructure.Test
             DbContext.SaveChanges();
 
             var efquery = new Query<Meal>(DbContext);
-            efquery.OrderBy<int>("Id", false);
+            efquery.Where<string>(a => a == "Rezen", "Name");
+            efquery.Where<string>(a => a == "Obalovane maso", "Description");
             var result = efquery.Execute();
 
-            Assert.True(result.Count() == 2);
-            Assert.True(result.First().Name == "Burger");
+            Assert.True(result.Count() == 1);
+            Assert.True(result.First().Id == 1);
         }
 
         [Test]
-        public void Query_Page_HappyPath()
+        public void Query_OrderBy_HappyPath()
         {
+
+            var efquery = new Query<Meal>(DbContext);
+            efquery.OrderBy<int>("Id", false);
+            var result = efquery.Execute();
+
+            Assert.True(result.Count() == 3);
+            Assert.True(result.First().Name == "Pizza");
+        }
+
+        [Test]
+        public void Query_WhereAndOrder_HappyPath()
+        {
+
             DbContext.Meal.Add(new Meal
             {
-                Id = 2,
+                Id = 4,
                 Name = "Burger",
-                Description = "Maso v zemli",
+                Description = "Maso v zemli so syrom",
                 Picture = "obrazok",
                 Price = (decimal)150.00,
                 Restaurants = new List<Restaurant>()
@@ -83,15 +117,18 @@ namespace RestaurantWebInfrastructure.Test
 
             DbContext.SaveChanges();
 
-            DbContext.Meal.Add(new Meal
-            {
-                Id = 3,
-                Name = "Pizza",
-                Description = "Maso v zemli",
-                Picture = "obrazok",
-                Price = (decimal)150.00,
-                Restaurants = new List<Restaurant>()
-            });
+            var efquery = new Query<Meal>(DbContext);
+            efquery.Where<string>(a => a == "Burger", "Name");
+            efquery.OrderBy<int>("Id", false);
+            var result = efquery.Execute();
+
+            Assert.True(result.Count() == 2);
+            Assert.True(result.First().Description == "Maso v zemli so syrom");
+        }
+
+        [Test]
+        public void Query_Page_HappyPath()
+        {
 
             DbContext.SaveChanges();
 
@@ -101,6 +138,68 @@ namespace RestaurantWebInfrastructure.Test
 
             Assert.True(result.Count() == 2);
             Assert.True(result.First().Name == "Rezen");
+        }
+
+        [Test]
+        public void Query_PageAndOrder_HappyPath()
+        {
+
+            var efquery = new Query<Meal>(DbContext);
+            efquery.Page(1, 2);
+            efquery.OrderBy<int>("Id", false);
+            var result = efquery.Execute();
+
+            Assert.True(result.Count() == 2);
+            Assert.True(result.First().Name == "Pizza");
+        }
+
+        [Test]
+        public void Query_PageAndWhere_HappyPath()
+        {
+
+            var efquery = new Query<Meal>(DbContext);
+            efquery.Page(1, 2);
+            efquery.Where<string>(a => a == "Rezen", "Name");
+            var result = efquery.Execute();
+
+            Assert.True(result.Count() == 1);
+            Assert.True(result.First().Name == "Rezen");
+        }
+
+        [Test]
+        public void Query_PageAndWhereandOrder_HappyPath()
+        {
+            DbContext.Meal.Add(new Meal
+            {
+                Id = 4,
+                Name = "Burger",
+                Description = "Maso v zemli so syrom",
+                Picture = "obrazok",
+                Price = (decimal)150.00,
+                Restaurants = new List<Restaurant>()
+            });
+
+            DbContext.Meal.Add(new Meal
+            {
+                Id = 5,
+                Name = "Burger",
+                Description = "Maso v zemli so syrom a salatom",
+                Picture = "obrazok",
+                Price = (decimal)180.00,
+                Restaurants = new List<Restaurant>()
+            });
+
+            DbContext.SaveChanges();
+
+            var efquery = new Query<Meal>(DbContext);
+            efquery.Where<string>(a => a == "Burger", "Name");
+            efquery.Page(1, 2);
+            efquery.OrderBy<int>("Id", true);
+            var result = efquery.Execute();
+
+            Assert.True(result.Count() == 2);
+            Assert.True(result.First().Price == (decimal)140.00);
+            Assert.True(result.First().Id == 2);
         }
 
     }

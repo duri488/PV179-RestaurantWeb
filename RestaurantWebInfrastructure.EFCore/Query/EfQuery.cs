86f2a7c8438c7e0d065fddce5e92f1281b5ffe1e
Juraj Lazorík <lazorik.juraj@gmail.com>
2022-10-22 17:55:32 +0200

    Split query code according to project structure changes

--- RestaurantWebInfrastructure.EFCore/Query/EfQuery.cs
+++ RestaurantWebInfrastructure.EFCore/Query/EfQuery.cs
@@ -1,42 +1,20 @@
 ﻿using System.Linq.Expressions;
 using RestaurantWebDAL;
+using RestaurantWebInfrastructure.EFCore.ExpressionHelpers;
 using RestaurantWebInfrastructure.Query;
 
 namespace RestaurantWebInfrastructure.EFCore.Query
 {
-    public class EfQuery<TEntity> : IQuery<TEntity> where TEntity : class, new()
+    public class EfQuery<TEntity> : Query<TEntity> where TEntity : class, new()
     {
         protected RestaurantWebDbContext Dbcontext { get; set; }
-
-        public List<(Expression expression, Type argumentType, string columnName)> WherePredicate { get; set; } = new();
-        public (string tableName, bool isAscending, Type argumentType)? OrderByContainer { get; set; }
-        public (int PageToFetch, int PageSize)? PaginationContainer { get; set; }
 
         public EfQuery(RestaurantWebDbContext dbcontext)
         {
             Dbcontext = dbcontext;
         }
 
-        public IQuery<TEntity> Page(int pageToFetch, int pageSize = 10)
-        {
-            PaginationContainer = (pageToFetch, pageSize);
-            return this;
-        }
-
-        public IQuery<TEntity> OrderBy<T>(string columnName, bool ascendingOrder = true) where T : IComparable<T>
-        {
-            OrderByContainer = (columnName, ascendingOrder, typeof(T));
-            return this;
-        }
-
-        // 1st argument -> table name
-        public IQuery<TEntity> Where<T>(Expression<Func<T, bool>> predicate, string columnName) where T : IComparable<T>
-        {
-            WherePredicate.Add((predicate, typeof(T), columnName));
-            return this;
-        }
-
-        public IEnumerable<TEntity> Execute()
+        public override IEnumerable<TEntity> Execute()
         {
             IQueryable<TEntity> query = Dbcontext.Set<TEntity>();
 
@@ -56,6 +34,7 @@ namespace RestaurantWebInfrastructure.EFCore.Query
             }
 
             return query.ToList();
+
         }
 
         private IQueryable<TEntity> ApplyWhere(IQueryable<TEntity> query)

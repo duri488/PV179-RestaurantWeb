2cb84935d4d61038321152b02e4cf6390838c885
Denis Lebo <d.lebo112@gmail.com>
2022-11-11 18:18:40 +0100

    Add IRepository contract

---
+++ RestaurantWebBL/Contracts/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace RestaurantWebBL.Contracts;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    TEntity GetById(int id);
+
+    void Insert(TEntity entity);
+
+    void Delete(object id);
+
+    void Delete(TEntity entityToDelete);
+
+    void Update(TEntity entityToUpdate);
+}

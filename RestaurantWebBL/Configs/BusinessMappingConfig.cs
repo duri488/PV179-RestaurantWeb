18db0e203ab53a523d6062466432eced8b165b17
Denis Lebo <d.lebo112@gmail.com>
2022-11-14 21:37:43 +0100

    Add mapping

--- RestaurantWebBL/Configs/BusinessMappingConfig.cs
+++ RestaurantWebBL/Configs/BusinessMappingConfig.cs
@@ -8,7 +8,13 @@ namespace RestaurantWebBL.Configs
     {
         public static void ConfigureMapping(IMapperConfigurationExpression config)
         {
+            config.CreateMap<DailyMenu, DailyMenuDto>().ReverseMap();
             config.CreateMap<Drink, DrinkDto>().ReverseMap();
+            config.CreateMap<Localization, LocalizationDto>().ReverseMap();
+            config.CreateMap<Meal, MealDto>().ReverseMap();
+            config.CreateMap<Restaurant, RestaurantDto>().ReverseMap();
+            config.CreateMap<User, UserDto>().ReverseMap();
+            config.CreateMap<WeeklyMenu, WeeklyMenuDto>().ReverseMap();
         }
     }
 }

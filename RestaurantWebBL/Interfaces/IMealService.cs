7e07c17052a3f98b1965d5e1282ff4435b28ad5a
xbock <xbock@fi.muni.cz>
2022-11-15 18:22:08 +0100

    fix IMealService

--- RestaurantWebBL/Interfaces/IMealService.cs
+++ RestaurantWebBL/Interfaces/IMealService.cs
@@ -5,12 +5,12 @@ namespace RestaurantWebBL.Interfaces
     public interface IMealService
     {
         Task CreateAsync(MealDto createdEntity);
-        Task<MealDto?> GetByIdAsync(int entityId);
-        Task UpdateAsync(int entityId, MealDto updatedEntity);
         Task DeleteAsync(int entityId);
         Task<IEnumerable<MealDto>> GetAllAsync();
-        Task<IEnumerable<MealDto>> GetMealsPriceIsBigger(decimal price);
-        Task<IEnumerable<MealDto>> GetMealsPriceIsLower(decimal price);
+        Task<MealDto?> GetByIdAsync(int entityId);
+        Task UpdateAsync(int entityId, MealDto updatedEntity);
+        Task<IEnumerable<MealDto>> GetMealsPriceIsBiggerAsync(decimal price);
+        Task<IEnumerable<MealDto>> GetMealsPriceIsLowerAsync(decimal price);
         Task<MealDto?> GetByNameAsync(string name);
     }
 }

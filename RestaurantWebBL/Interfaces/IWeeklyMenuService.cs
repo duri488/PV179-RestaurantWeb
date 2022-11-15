f8afb5ae0c0aaa05905ad2d126954b8de5ad9d7e
Denis Lebo <d.lebo112@gmail.com>
2022-11-16 00:22:13 +0100

    Add WeeklyMenuService

---
+++ RestaurantWebBL/Interfaces/IWeeklyMenuService.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantWebBL.DTOs;
+
+namespace RestaurantWebBL.Interfaces;
+
+public interface IWeeklyMenuService
+{
+    Task CreateAsync(WeeklyMenuDto createdEntity);
+    Task<WeeklyMenuDto?> GetByIdAsync(int entityId);
+    Task UpdateAsync(WeeklyMenuDto updatedEntity);
+    Task DeleteAsync(int entityId);
+    Task<IEnumerable<WeeklyMenuDto>> GetAllAsync();
+}
---
+++ RestaurantWebBL/Services/WeeklyMenuService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using RestaurantWeb.Contract;
+using RestaurantWebBL.DTOs;
+using RestaurantWebBL.Interfaces;
+using RestaurantWebDAL.Models;
+
+namespace RestaurantWebBL.Services;
+
+public class WeeklyMenuService : IWeeklyMenuService
+{
+    private readonly IRepository<WeeklyMenu> _weeklyMenuRepository;
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public WeeklyMenuService(IRepository<WeeklyMenu> weeklyMenuRepository,
+        IMapper mapper,
+        IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _weeklyMenuRepository = weeklyMenuRepository;
+        _mapper = mapper;
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task CreateAsync(WeeklyMenuDto createdEntity)
+    {
+        var weeklyMenu = _mapper.Map<WeeklyMenu>(createdEntity);
+        await using IUnitOfWork unitOfWork = _unitOfWorkFactory.Build();
+        _weeklyMenuRepository.Insert(weeklyMenu);
+        await unitOfWork.CommitAsync();
+    }
+
+    public async Task<WeeklyMenuDto?> GetByIdAsync(int entityId)
+    {
+        WeeklyMenu? dailyMenu = await _weeklyMenuRepository.GetByIdAsync(entityId);
+        return _mapper.Map<WeeklyMenuDto?>(dailyMenu);
+    }
+
+    public async Task UpdateAsync(WeeklyMenuDto updatedEntity)
+    {
+        var updatedModel = _mapper.Map<WeeklyMenu>(updatedEntity);
+        await using IUnitOfWork unitOfWork = _unitOfWorkFactory.Build();
+        _weeklyMenuRepository.Update(updatedModel);
+        await unitOfWork.CommitAsync();
+    }
+
+    public async Task DeleteAsync(int entityId)
+    {
+        await using IUnitOfWork unitOfWork = _unitOfWorkFactory.Build();
+        await _weeklyMenuRepository.DeleteAsync(entityId);
+        await unitOfWork.CommitAsync();
+    }
+
+    public async Task<IEnumerable<WeeklyMenuDto>> GetAllAsync()
+    {
+        IEnumerable<WeeklyMenu> dailyMenuList = await _weeklyMenuRepository.GetAllAsync();
+        return _mapper.Map<IEnumerable<WeeklyMenuDto>>(dailyMenuList);
+    }
+}

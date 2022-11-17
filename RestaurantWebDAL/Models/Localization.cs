037229f6fa9db8c0f41f62d3159f069c8a115f07
xbock <xbock@fi.muni.cz>
2022-11-17 15:52:36 +0100

    fix unique in localization

--- RestaurantWebDAL/Models/Localization.cs
+++ RestaurantWebDAL/Models/Localization.cs
@@ -5,7 +5,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RestaurantWebDAL.Models
 {
-    [Index(nameof(StringCode),IsUnique = true)]
     public class Localization : BaseEntity
     {
         [MaxLength(2)]

01d65eaee11c7457f66031a8ee8874028465a85a
Denis Lebo <d.lebo112@gmail.com>
2022-12-09 03:41:07 +0100

    Add DisplayName properties

--- PV179-RestaurantWeb/Models/DailyMenuViewModel.cs
+++ PV179-RestaurantWeb/Models/DailyMenuViewModel.cs
@@ -1,13 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace PV179_RestaurantWeb.Models;
 
 public class DailyMenuViewModel
 {
     public int Id { get; set; }
+    [DisplayName("Day")]
     public DayOfWeek DayOfWeek { get; set; }
     [DataType(DataType.Date)]
     public DateTime Date { get; set; }
+    [DataType(DataType.Currency)]
+    [DisplayName("Price")]
     public decimal MenuPrice { get; set; }
     public MealViewModel Meal { get; set; }
 }

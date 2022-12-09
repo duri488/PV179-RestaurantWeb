ad9ca4057ecaf7344ceea913cdce92806a33d1f5
xbock <xbock@fi.muni.cz>
2022-12-09 22:13:00 +0100

    Create drink view

---
+++ PV179-RestaurantWeb/Models/DrinkCreateModel.cs
@@ -0,0 +1,25 @@
+﻿using RestaurantWebBL.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace PV179_RestaurantWeb.Models
+{
+    public class DrinkCreateModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Volume { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public IEnumerable<AllergenViewModel> Allergens { get; set; }
+
+        public DrinkDto drinkDto() => new()
+        {
+            Id = Id,
+            Name = Name,
+            Volume = Volume,
+            Price = Price,
+        };
+    }
+}

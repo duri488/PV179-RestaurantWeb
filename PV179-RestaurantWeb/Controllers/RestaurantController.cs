62ce2ce436a0eee8b05ec0a538ff755b6ab5b8e7
Juraj Lazorík <lazorik.juraj@gmail.com>
2022-12-09 21:02:36 +0100

    Use correct DI in Restaurant Controller

--- PV179-RestaurantWeb/Controllers/RestaurantController.cs
+++ PV179-RestaurantWeb/Controllers/RestaurantController.cs
@@ -2,32 +2,23 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PV179_RestaurantWeb.Models;
-using RestaurantWeb.Contract;
-using RestaurantWebBL.Services;
-using RestaurantWebDAL.Models;
+using RestaurantWebBL.Interfaces;
 
 namespace PV179_RestaurantWeb.Controllers
 {
     public class RestaurantController : Controller
     {
-        private readonly IRepository<Restaurant> _restaurantRepository;
-        private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+        private readonly IRestaurantService _restaurantService;
         private readonly IMapper _mapper;
 
-        private readonly ILogger<RestaurantController> _logger;
-
-        public RestaurantController(ILogger<RestaurantController> logger, IRepository<Restaurant> restaurantRepository, IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
+        public RestaurantController(IMapper mapper, IRestaurantService restaurantService)
         {
-            _restaurantRepository = restaurantRepository;
-            _unitOfWorkFactory = unitOfWorkFactory;
+            _restaurantService = restaurantService;
             _mapper = mapper;
-            _logger = logger;
         }
         public async Task<IActionResult> Index()
         {
-            var restaurantService = new RestaurantService(_unitOfWorkFactory, _mapper, _restaurantRepository);
-
-            var restaurants = await restaurantService.GetAllAsync();
+            var restaurants = await _restaurantService.GetAllAsync();
             var restaurant = restaurants.FirstOrDefault();
 
             var model = _mapper.Map<RestaurantViewModel>(restaurant);

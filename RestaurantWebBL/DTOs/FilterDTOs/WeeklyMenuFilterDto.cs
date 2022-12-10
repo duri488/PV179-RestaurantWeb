86ff25b9b634b7e1291315e3872fbf46f95af536
Denis Lebo <d.lebo112@gmail.com>
2022-12-10 10:43:14 +0100

    Add query object for weekly menus

---
+++ RestaurantWebBL/DTOs/FilterDTOs/WeeklyMenuFilterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantWebBL.DTOs.FilterDTOs;
+
+public class WeeklyMenuFilterDto
+{
+    public DateTime DateFrom { get; set; }
+    public DateTime DateTo { get; set; }
+    public int? RequestedPageNumber { get; set; }
+    public int PageSize { get; set; } = 10;
+    public string SortCriteria { get; set; } = "";
+    public bool SortAscending { get; set; }
+}
---
+++ RestaurantWebBL/QueryObjects/WeeklyMenuQueryObject.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using RestaurantWeb.Contract;
+using RestaurantWebBL.DTOs;
+using RestaurantWebBL.DTOs.FilterDTOs;
+using RestaurantWebDAL.Models;
+
+namespace RestaurantWebBL.QueryObjects;
+
+public class WeeklyMenuQueryObject
+{
+    private readonly IQuery<WeeklyMenu> _query;
+    private readonly IMapper _mapper;
+
+    public WeeklyMenuQueryObject(IQuery<WeeklyMenu> query, IMapper mapper)
+    {
+        _query = query;
+        _mapper = mapper;
+    }
+
+    public QueryResultDto<WeeklyMenuDto> WeeklyMenusInDateRange(WeeklyMenuFilterDto filter)
+    {
+        IQuery<WeeklyMenu> query = _query
+            .Where<DateTime>(a => a >= filter.DateFrom, nameof(WeeklyMenu.DateFrom))
+            .Where<DateTime>(a => a <= filter.DateTo, nameof(WeeklyMenu.DateTo));
+
+        if (!string.IsNullOrWhiteSpace(filter.SortCriteria))
+        {
+            query = query.OrderBy<string>(filter.SortCriteria, filter.SortAscending);
+        }
+        if (filter.RequestedPageNumber.HasValue)
+        {
+            query = query.Page(filter.RequestedPageNumber.Value, filter.PageSize);
+        }
+
+        IEnumerable<WeeklyMenu> items = query.Execute();
+        IEnumerable<WeeklyMenuDto> weeklyMenuDtos = _mapper.Map<IEnumerable<WeeklyMenuDto>>(items).ToList();
+     
+        return new QueryResultDto<WeeklyMenuDto>
+        {
+            Items = weeklyMenuDtos,
+            PageSize = filter.PageSize,
+            RequestedPageNumber = filter.RequestedPageNumber,
+            TotalItemsCount = weeklyMenuDtos.Count()
+        };
+    }
+}

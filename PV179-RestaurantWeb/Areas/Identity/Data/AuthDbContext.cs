24882d291d91ac8d4be1cdc674c826c3cac56d4d
Juraj Lazorík <lazorik.juraj@gmail.com>
2022-12-10 19:19:41 +0100

    Add Identity framework and seed admin user

---
+++ PV179-RestaurantWeb/Areas/Identity/Data/AuthDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PV179_RestaurantWeb.Areas.Identity.Data;
+
+public class AuthDbContext : IdentityDbContext<AdminUser>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override async void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        var hasher = new PasswordHasher<AdminUser>();
+
+        var admin = new AdminUser()
+        {
+            Id = Guid.NewGuid().ToString(),
+            UserName = "lazorik.juraj@gmail.com",
+            FirstName = "Gordon",
+            LastName = "Ramsay",
+            NormalizedUserName = "lazorik.juraj@gmail.com",
+            Email = "lazorik.juraj@gmail.com",
+            NormalizedEmail = "lazorik.juraj@gmail.com",
+            EmailConfirmed = true,
+            SecurityStamp = string.Empty,
+            PhoneNumber = "0917123456",
+            PhoneNumberConfirmed = true
+        };
+        admin.PasswordHash = hasher.HashPassword(admin, "admin");
+
+
+        builder.Entity<AdminUser>().HasData(admin);
+    }
+}

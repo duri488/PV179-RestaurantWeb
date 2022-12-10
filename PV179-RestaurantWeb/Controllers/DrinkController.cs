5bfc302dadc9da5460e06ce9d943490026ff3d8c
xbock <xbock@fi.muni.cz>
2022-12-10 15:45:55 +0100

    delete drink

--- PV179-RestaurantWeb/Controllers/DrinkController.cs
+++ PV179-RestaurantWeb/Controllers/DrinkController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using PV179_RestaurantWeb.Models;
 using RestaurantWebBL.DTOs;
 using RestaurantWebBL.Interfaces;
+using RestaurantWebBL.Services;
 
 namespace PV179_RestaurantWeb.Controllers
 {
@@ -10,10 +11,12 @@ namespace PV179_RestaurantWeb.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IDrinkService _drinkService;
-        public DrinkController(IMapper mapper, IDrinkService drinkService)
+        private readonly IAllergenService _allergenService;
+        public DrinkController(IMapper mapper, IDrinkService drinkService, IAllergenService allergenService)
         {
             _mapper = mapper;
             _drinkService = drinkService;
+            _allergenService = allergenService;
         }
 
         public async Task<IActionResult> Index()
@@ -23,9 +26,61 @@ namespace PV179_RestaurantWeb.Controllers
             return View(drinkViewModels);
         }
 
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            DrinkDto? drinkDto = await _drinkService.GetByIdAsync(id.Value);
+
+            if (drinkDto == null)
+            {
+                return NotFound();
+            }
+
+            var drink = _mapper.Map<DrinkViewModel>(drinkDto);
+            IEnumerable<AllergenDto> allergenDtos = await _allergenService.GetByFlags(drinkDto.AllergenFlags);
+            var allergens = _mapper.Map<IEnumerable<AllergenViewModel>>(allergenDtos);
+            drink.Allergens = allergens;
+            return View(drink);
+        }
+
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(DrinkCreateModel model)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var dto = model.drinkDto();
+            _drinkService.CreateAsync(dto, 1);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var dto = _drinkService.GetByIdAsync(id);
+            if (dto == null)
+            {
+                return NotFound();
+            }
+
+            _drinkService.DeleteAsync(id);
+
+            return RedirectToAction(nameof(Index));
         }
 
     }
--- PV179-RestaurantWeb/Models/DrinkViewModel.cs
+++ PV179-RestaurantWeb/Models/DrinkViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class DrinkViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Volume { get; set; }
         public decimal Price { get; set; }

6d6e9d27c8ff9b01194e77d59980cbfc515e0e92
Denis Lebo <d.lebo112@gmail.com>
2022-12-10 14:19:42 +0100

    Implement QueryFactory

---
+++ RestaurantWeb.Contract/IQueryFactory.cs
@@ -0,0 +1,6 @@
+﻿namespace RestaurantWeb.Contract;
+
+public interface IQueryFactory<TEntity> where TEntity : class, new()
+{
+    IQuery<TEntity> Build();
+}
---
+++ RestaurantWebInfrastructure.EFCore/Query/EfQueryFactory.cs
@@ -0,0 +1,19 @@
+﻿using RestaurantWeb.Contract;
+using RestaurantWebDAL;
+
+namespace RestaurantWebInfrastructure.EFCore.Query;
+
+public class EfQueryFactory<TEntity> : IQueryFactory<TEntity> where TEntity : class, new()
+{
+    private readonly RestaurantWebDbContext _context;
+
+    public EfQueryFactory(RestaurantWebDbContext context)
+    {
+        _context = context;
+    }
+
+    public IQuery<TEntity> Build()
+    {
+        return new EfQuery<TEntity>(_context);
+    }
+}

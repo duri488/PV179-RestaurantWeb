15e334a64a2cb22757938080ed10491efd913ea8
Denis Lebo <d.lebo112@gmail.com>
2022-12-10 11:10:17 +0100

    Add Menu Facade

---
+++ RestaurantWebBL/Facades/MenuFacade.cs
@@ -0,0 +1,38 @@
+﻿using RestaurantWebBL.DTOs;
+using RestaurantWebBL.Interfaces;
+
+namespace RestaurantWebBL.Facades;
+
+public class MenuFacade : IMenuFacade
+{
+    private readonly IMealService _mealService;
+    private readonly IWeeklyMenuService _weeklyMenuService;
+    private readonly IDailyMenuService _dailyMenuService;
+
+    public MenuFacade(IMealService mealService, IWeeklyMenuService weeklyMenuService, IDailyMenuService dailyMenuService)
+    {
+        _mealService = mealService;
+        _weeklyMenuService = weeklyMenuService;
+        _dailyMenuService = dailyMenuService;
+    }
+
+    public async Task<IEnumerable<MealDto>> GetAllMealsAsync()
+    {
+        return await _mealService.GetAllAsync();
+    }
+
+    public async Task<IEnumerable<DailyMenuDto>> GetAllDailyMenusAsync()
+    {
+        return await _dailyMenuService.GetAllAsync();
+    }
+
+    public async Task<IEnumerable<DailyMenuDto>> GetAllDailyMenusAsync(bool includeNavigationProperty)
+    {
+        return await _dailyMenuService.GetAllAsync(includeNavigationProperty);
+    }
+
+    public async Task<IEnumerable<WeeklyMenuDto>> GetAllWeeklyMenusAsync()
+    {
+        return await _weeklyMenuService.GetAllAsync();
+    }
+}
---
+++ RestaurantWebBL/Interfaces/IMenuFacade.cs
@@ -0,0 +1,11 @@
+﻿using RestaurantWebBL.DTOs;
+
+namespace RestaurantWebBL.Interfaces;
+
+public interface IMenuFacade
+{
+    Task<IEnumerable<MealDto>> GetAllMealsAsync();
+    Task<IEnumerable<DailyMenuDto>> GetAllDailyMenusAsync();
+    Task<IEnumerable<DailyMenuDto>> GetAllDailyMenusAsync(bool includeNavigationProperty);
+    Task<IEnumerable<WeeklyMenuDto>> GetAllWeeklyMenusAsync();
+}

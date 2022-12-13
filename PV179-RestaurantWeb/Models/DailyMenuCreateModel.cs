d663973435d796328602318f3a6c142eb932fd39
Denis Lebo <d.lebo112@gmail.com>
2022-12-13 18:31:32 +0100

    Change style of daily menus

--- PV179-RestaurantWeb/Models/DailyMenuCreateModel.cs
+++ PV179-RestaurantWeb/Models/DailyMenuCreateModel.cs
@@ -13,11 +13,13 @@ public class DailyMenuCreateModel
     [DataType(DataType.Currency)]
     [DisplayName("Price")]
     public decimal MenuPrice { get; set; }
+    public IEnumerable<MealDto>? Meal { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "Value must be greater than 0")]
     [Required(ErrorMessage = "This field is required")]
-    public IEnumerable<MealDto> Meal { get; set; }
-    public int MealId { get; set; }
+    public int? MealId { get; set; }
     [DisplayName("Week")]
+    public IEnumerable<WeeklyMenuDto>? WeeklyMenu { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "Value must be greater than 0")]
     [Required(ErrorMessage = "This field is required")]
-    public IEnumerable<WeeklyMenuDto> WeeklyMenu { get; set; }
-    public int WeeklyMenuId { get; set; }
+    public int? WeeklyMenuId { get; set; }
 }

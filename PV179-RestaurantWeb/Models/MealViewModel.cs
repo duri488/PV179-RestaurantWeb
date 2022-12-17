aa308e57a9bd54eb4ef859a449f60abe8b84afed
xbock <xbock@fi.muni.cz>
2022-12-17 12:10:04 +0100

    Change meal view model

--- PV179-RestaurantWeb/Models/MealViewModel.cs
+++ PV179-RestaurantWeb/Models/MealViewModel.cs
@@ -2,6 +2,7 @@
 
 public class MealViewModel
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public decimal Price { get; set; }
     public string Description { get; set; }
